494d170e019e7e614d2997e28a74c596721aa692
Arran Jacques <arranjacques@gmail.com>
2019-09-04 13:16:49 +0100

    Remove get initial props from document component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,6 @@
-import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class extends Document {
-
-    static async getInitialProps(ctx: DocumentContext) {
-        const initialProps = await Document.getInitialProps(ctx);
-        return { ...initialProps };
-    }
 
     render() {
         return (
