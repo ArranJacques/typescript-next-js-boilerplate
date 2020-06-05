f3dbcbedb087a8c34b2c099449f9ae46abe45974
Arran Jacques <arranjacques@gmail.com>
2020-06-05 14:18:15 +0100

    Add seo tags

--- app/presentation/components/4-layouts/base/BaseLayout.tsx
+++ app/presentation/components/4-layouts/base/BaseLayout.tsx
@@ -1,22 +1,43 @@
-import Head from 'next/head';
+import { Record } from 'immutable';
+import { NextSeo } from 'next-seo';
 import React, { PureComponent } from 'react';
 import { PageProps } from 'support/page';
 
 interface Props extends PageProps {
     className?: string
+    seo?: Record<{
+        title?: string
+        description?: string
+    }>
 }
 
 export default class extends PureComponent<Props> {
 
+    protected getOpenGraphProperties(): { [key: string]: any } {
+
+        const { pageContext, seo } = this.props;
+
+        return {
+            description: seo ? seo.get('description') : undefined,
+            site_name: 'Hello World',
+            title: seo ? seo.get('title') : 'Hello World',
+            type: 'website',
+            url: pageContext.get('url').get('canonical')
+        };
+    }
+
     public render(): JSX.Element {
 
-        const { children, className } = this.props;
+        const { children, className, pageContext, seo } = this.props;
 
         return (
             <>
-                <Head>
-                    <title>Hello World</title>
-                </Head>
+                <NextSeo
+                    title={seo ? seo.get('title') : 'Hello World'}
+                    canonical={pageContext.get('url').get('canonical')}
+                    description={seo ? seo.get('description') : undefined}
+                    openGraph={this.getOpenGraphProperties()}
+                />
                 <div className={`lyt-base${className ? ' ' + className : ''}`}>
                     {children}
                 </div>
