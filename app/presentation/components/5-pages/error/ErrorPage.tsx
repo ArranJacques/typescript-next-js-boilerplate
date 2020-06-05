0527dcf69b9efab67ad0671e96a3b0c90ed67cfb
Arran Jacques <arranjacques@gmail.com>
2020-06-05 12:47:36 +0100

    Improvements to error page

--- app/presentation/components/5-pages/error/ErrorPage.tsx
+++ app/presentation/components/5-pages/error/ErrorPage.tsx
@@ -1,33 +1,40 @@
+import { bemClassList } from 'frontend-utilities';
+import { NextPageContext } from 'next/dist/next-server/lib/utils';
 import BaseLayout from 'presentation/composed/4-layouts/BaseLayout';
 import React, { PureComponent } from 'react';
+import { DeviceProps, getDeviceModifiers } from 'support/device-detect';
 import { PageProps } from 'support/page';
-import { NextJSReduxPageContext } from 'support/types';
 
-interface Props extends PageProps {
+interface InitialProps {
     statusCode: number | null
+}
+
+interface Props extends DeviceProps, InitialProps, PageProps {
+    //
 }
 
 export default class extends PureComponent<Props> {
 
-    public static async getInitialProps({ res, err }: NextJSReduxPageContext) {
-        const statusCode = res ? res.statusCode : err ? err.statusCode : null;
+    static async getInitialProps({ res, err }: NextPageContext): Promise<InitialProps> {
+
+        const statusCode = !(res && res.statusCode)
+            ? (err && err.statusCode) ? err.statusCode : null
+            : res.statusCode;
+
         return { statusCode };
     }
 
     public render(): JSX.Element {
 
-        const { statusCode } = this.props;
+        const { device, statusCode } = this.props;
+        const cl = bemClassList('pg-error').add(getDeviceModifiers(device));
 
         return (
-            <BaseLayout className="pg-error">
-                {!statusCode ? (
-                    <h1>An error occurred</h1>
-                ) : (
-                    <>
-                        <h1>{statusCode}</h1>
-                        <h2>{statusCode === 404 ? 'Page not found' : 'An error occurred on server'}</h2>
-                    </>
-                )}
+            <BaseLayout className={cl.string()}>
+                <div className="pg-error__body">
+                    <h1>{statusCode ? statusCode : 'Unknown'}</h1>
+                    <h2>{statusCode === 404 ? 'Page not found' : 'An error occurred'}</h2>
+                </div>
             </BaseLayout>
         );
     }
