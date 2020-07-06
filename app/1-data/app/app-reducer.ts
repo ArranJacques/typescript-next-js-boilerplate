0b800b9143531ab16dae61d537381c33388e95b8
Arran Jacques <arranjacques@gmail.com>
2020-07-06 08:42:44 +0100

    Add better type hinting to reducer

--- app/1-data/app/app-reducer.ts
+++ app/1-data/app/app-reducer.ts
@@ -16,11 +16,22 @@ export const State = Record<Props>(defaultValues);
 
 export default (state: AppState = new State(), action: app.Action | AnyAction): AppState => {
 
-    switch (action.type) {
-        case HYDRATE:
-            return action.payload.app;
+    // TypeScript gets a bit confused sometimes if the "payload" property doesn't
+    // exist on one or more of the action types we define. By assigning the "action"
+    // argument to a variable and explicitly defining it's type, as done below, it
+    // helps TypeScript get a grip on what's going on and prevents it throwing
+    // errors...
+
+    if (action.type === HYDRATE) {
+        const any = action as AnyAction;
+        return any.payload.app;
+    }
+
+    const a = action as app.Action;
+
+    switch (a.type) {
         case app.ActionType.SetHello:
-            return state.set('hello', action.payload);
+            return state.set('hello', a.payload);
     }
 
     return state;
